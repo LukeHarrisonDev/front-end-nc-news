5a9b29c965fb60f7ff2efc64ff56311d65ba4ca1
BassManLuke <luke_haz@yahoo.co.uk>
2024-08-09 11:20:27 +0100

    deploy the app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ function Home() {
     return (
       <>
         <section>
-          <img id="stock" src="src/images/news-paper.jpg" />
-          <img id="stock" src="src/images/news-phone.jpg" />
+          <img id="stock" src="./src/images/news-paper.jpg" />
+          <img id="stock" src="./src/images/news-phone.jpg" />
         </section>
         <h2>Welcome to NC News</h2>
         <p>Ay Up, welcome tu' best News ar'icle website in' North</p>
